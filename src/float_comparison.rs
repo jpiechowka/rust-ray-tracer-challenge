dd7098eee1a9fed76c4f1dab79a889fcc39ace0a
Janusz Piechówka <januszpiechowka@gmail.com>
2023-09-07 23:54:58 +0200

    Finish chapter 1 implementation of tuples (points and vectors) with tests

--- src/float_comparison.rs
+++ src/float_comparison.rs
@@ -1,6 +1,6 @@
 const MARGIN: f64 = 0.00001;
 
 #[inline(always)]
-pub fn compare_f64_with_margin(a: f64, b: f64) -> bool {
+pub fn is_equal_f64_with_margin(a: f64, b: f64) -> bool {
     (a - b).abs() < MARGIN
 }
